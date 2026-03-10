249568a510c40868cad7619d79093fd3802ca8ad
MELANIE GARDEUX <melanie.gardeux54@gmail.com>
2026-03-10 09:43:10 +0100

    Ajout de la détection automatique du damier avec OpenCV
    Ajoute la détection des coins d’un damier dans la fonction calibration() via findChessboardCorners, affiche le résultat et dessine les coins si le damier est détecté. Inclusion de l’en-tête calib3d d’OpenCV.

--- src/calibration.cpp
+++ src/calibration.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <filesystem>
+#include <opencv2/calib3d.hpp>
 
 #include "calibration.hpp"
 
@@ -23,6 +24,28 @@ void calibration()
 	cv::resize(image, image, cv::Size(), 0.5, 0.5);
 
 	// afficher l'image
+	cv::imshow("Damier", image);
+	cv::waitKey(0);
+
+	// taille du damier (coins internes)
+	cv::Size patternSize(9, 6);
+
+	// conteneur pour les coins
+	std::vector<cv::Point2f> corners;
+
+	// detection du damier
+	bool found = cv::findChessboardCorners(image, patternSize, corners);
+
+	if (found)
+	{
+		std::cout << "Damier detecte\n";
+		cv::drawChessboardCorners(image, patternSize, corners, found);
+	}
+	else
+	{
+		std::cout << "Damier non detecte\n";
+	}
+
 	cv::imshow("Damier", image);
 	cv::waitKey(0);
 
