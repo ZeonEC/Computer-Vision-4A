eba32c39fc5ddebaf03eb89ac12138280cad44aa
MELANIE GARDEUX <melanie.gardeux54@gmail.com>
2026-03-16 17:59:30 +0100

    Ajout des fonctions XML pour la calibration caméra
    Suppression de gestion_XML(). Ajout de save_calibration_xml et get_calibration_from_xml pour sauvegarder et charger tous les paramètres de calibration de caméra dans des fichiers XML. Facilite la gestion complète des données de calibration.

--- include/gestion_XML.hpp
+++ include/gestion_XML.hpp
@@ -1,3 +1,25 @@
 #pragma once
 
-void gestion_XML();
+void save_calibration_xml(const std::string& filename,
+	const cv::Mat& camera_matrix,
+	const cv::Mat& dist_coeffs,
+	const std::vector<cv::Mat>& rvecs,
+	const std::vector<cv::Mat>& tvecs,
+	const std::vector<double>& perViewErrors,
+	double RMS,
+	const cv::Size& taille_image,
+	const cv::Size& pattern_size,
+	float square_size,
+	const std::vector<std::vector<cv::Point2f>>& image_points);
+
+void get_calibration_from_xml(const std::string& filename,
+	cv::Mat& camera_matrix,
+	cv::Mat& dist_coeffs,
+	std::vector<cv::Mat>& rvecs,
+	std::vector<cv::Mat>& tvecs,
+	std::vector<double>& perViewErrors,
+	double& RMS,
+	cv::Size& taille_image,
+	cv::Size& pattern_size,
+	float& square_size,
+	std::vector<std::vector<cv::Point2f>>& image_points);
