df657c4f8d70d2eb3ea77b51b55a0c2be8161aa8
MELANIE GARDEUX <melanie.gardeux54@gmail.com>
2026-03-23 09:46:35 +0100

    Ajout des fonctions XML pour calibration stéréo
    Ajout des fonctions save_stereo_calibration_xml et get_stereo_calibration_from_xml pour gérer la sauvegarde et la lecture des paramètres de calibration stéréo dans des fichiers XML. Amélioration de la lisibilité des signatures des fonctions existantes en réorganisant les arguments sur plusieurs lignes.

--- include/gestion_XML.hpp
+++ include/gestion_XML.hpp
@@ -4,27 +4,46 @@
 #include <vector>
 #include <opencv2/opencv.hpp>
 
-
 void save_calibration_xml(const std::string& filename,
-	const cv::Mat& camera_matrix,
-	const cv::Mat& dist_coeffs,
-	const std::vector<cv::Mat>& rvecs,
-	const std::vector<cv::Mat>& tvecs,
-	const std::vector<double>& perViewErrors,
-	double RMS,
-	const cv::Size& taille_image,
-	const cv::Size& pattern_size,
-	float square_size,
-	const std::vector<std::vector<cv::Point2f>>& image_points);
+    const cv::Mat& camera_matrix,
+    const cv::Mat& dist_coeffs,
+    const std::vector<cv::Mat>& rvecs,
+    const std::vector<cv::Mat>& tvecs,
+    const std::vector<double>& perViewErrors,
+    double RMS,
+    const cv::Size& taille_image,
+    const cv::Size& pattern_size,
+    float square_size,
+    const std::vector<std::vector<cv::Point2f>>& image_points);
 
 void get_calibration_from_xml(const std::string& filename,
-	cv::Mat& camera_matrix,
-	cv::Mat& dist_coeffs,
-	std::vector<cv::Mat>& rvecs,
-	std::vector<cv::Mat>& tvecs,
-	std::vector<double>& perViewErrors,
-	double& RMS,
-	cv::Size& taille_image,
-	cv::Size& pattern_size,
-	float& square_size,
-	std::vector<std::vector<cv::Point2f>>& image_points);
+    cv::Mat& camera_matrix,
+    cv::Mat& dist_coeffs,
+    std::vector<cv::Mat>& rvecs,
+    std::vector<cv::Mat>& tvecs,
+    std::vector<double>& perViewErrors,
+    double& RMS,
+    cv::Size& taille_image,
+    cv::Size& pattern_size,
+    float& square_size,
+    std::vector<std::vector<cv::Point2f>>& image_points);
+
+void save_stereo_calibration_xml(const std::string& filename,
+    const cv::Mat& camera_matrix1,
+    const cv::Mat& dist_coeffs1,
+    const cv::Mat& camera_matrix2,
+    const cv::Mat& dist_coeffs2,
+    const cv::Mat& R,
+    const cv::Mat& T,
+    const cv::Mat& E,
+    const cv::Mat& F);
+
+void get_stereo_calibration_from_xml(const std::string& filename,
+    cv::Mat& camera_matrix1,
+    cv::Mat& dist_coeffs1,
+    cv::Mat& camera_matrix2,
+    cv::Mat& dist_coeffs2,
+    cv::Mat& R,
+    cv::Mat& T,
+    cv::Mat& E,
+    cv::Mat& F);
